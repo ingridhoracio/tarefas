acb6f7a1730fb5ba56cdcd3a6de344b72c8aed74
evaldo <evaldo.junior@etec.sp.gov.br>
2023-08-14 15:05:51 -0300

    Incluindo o DateTimePicker

--- lib/tarefas_form.dart
+++ lib/tarefas_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TarefasForm extends StatefulWidget {
   const TarefasForm({super.key});
@@ -8,6 +9,8 @@ class TarefasForm extends StatefulWidget {
 }
 
 class _TarefasFormState extends State<TarefasForm> {
+  final _dateController = TextEditingController(); // controla o estado do campo da data
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -21,6 +24,21 @@ class _TarefasFormState extends State<TarefasForm> {
             ),
             SizedBox(height: 10,),
             TextFormField(
+              controller: _dateController, // controlador associado
+              readOnly: true,
+              onTap: () async {
+               var data = await showDatePicker(context: context,
+               
+               initialDate: DateTime.now(), 
+               firstDate: DateTime.now(), 
+               lastDate: DateTime.now().add(Duration(days: 365)),);
+                print(data);
+                if (data!=null) {
+                  String dataFormatada = DateFormat("dd/MM/yyyy").format(data);
+                  _dateController.text = dataFormatada;
+                }
+              } ,
+
               keyboardType: TextInputType.datetime,
               decoration: InputDecoration(
                   label: Text("Prazo"), border: OutlineInputBorder()),
