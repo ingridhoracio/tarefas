83c0cf0a9f59ac14fcdd11edc89918370ce0bedf
evaldo <evaldo.junior@etec.sp.gov.br>
2023-08-21 14:29:09 -0300

    Executar Validação.

--- lib/tarefas_form.dart
+++ lib/tarefas_form.dart
@@ -11,18 +11,23 @@ class TarefasForm extends StatefulWidget {
 class _TarefasFormState extends State<TarefasForm> {
   final _dateController = TextEditingController(); // controla o estado do campo da data
 
+  final _formKey = GlobalKey<FormState>(); // 1- Controla o estado do formulário
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
       child: Form(
+        
+        key: _formKey, //2- relaciona o controlador de estado do formulário
+        
         child: Column(
           children: [
             TextFormField(
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                   label: Text("Descrição"), border: OutlineInputBorder()),
             ),
-            SizedBox(height: 10,),
+            const SizedBox(height: 10,),
             TextFormField(
               controller: _dateController, // controlador associado
               readOnly: true,
@@ -31,7 +36,7 @@ class _TarefasFormState extends State<TarefasForm> {
                
                initialDate: DateTime.now(), 
                firstDate: DateTime.now(), 
-               lastDate: DateTime.now().add(Duration(days: 365)),);
+               lastDate: DateTime.now().add(const Duration(days: 365)),);
                 print(data);
                 if (data!=null) {
                   String dataFormatada = DateFormat("dd/MM/yyyy").format(data);
@@ -40,11 +45,20 @@ class _TarefasFormState extends State<TarefasForm> {
               } ,
 
               keyboardType: TextInputType.datetime,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                   label: Text("Prazo"), border: OutlineInputBorder()),
             ),
-            SizedBox(height: 10,),
-            ElevatedButton(onPressed: (){}, child: Text("Salvar"))
+            const SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              
+              //3- Executa a validação do formulário
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save(); // 4-Solicita ao formulário que salve os dados
+
+              }
+
+
+            }, child: const Text("Salvar"))
           ],
         ),
       ),
