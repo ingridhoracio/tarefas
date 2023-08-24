96dcfbb418d24059cd3f7729f9c7c6fd0140d803
ingrid <ingrid>
2023-08-24 13:21:05 -0300

    estado dos widgets

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:tarefas/tarefas_novo.dart';
 import 'package:tarefas/tarefas_page.dart';
+import 'package:tarefas/tarefas_state.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TarefaState state  = TarefaState(); 
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.orange),
       initialRoute: "/",
-      routes: {"/":(context)=>TarefasPage(),
-               "/add":(context)=>TarefasNovo()},
+      routes: {"/":(context)=>TarefasPage(state: state,),
+               "/add":(context)=>TarefasNovo(state: state,)},
       
     );
   }
